4f726271bf0b39569dda8d9ec393e7319883b858
shapp <abdo.sf.ahmed@gmail.com>
2020-05-27 17:22:04 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import './Product_Manager.dart';
+
+main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Easy List"),
+        ),
+        body: ProductManager('Food Tester')
+      ),
+    );
+  }
+}
+
+
